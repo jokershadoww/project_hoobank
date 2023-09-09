b905151050e24363cc29d9735aee55e38c458809
jokershadoww <134153914+jokershadoww@users.noreply.github.com>
2023-09-10 00:22:28 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,15 +9,15 @@ const Hero = () => {
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">20%</span> Discount For{" "}
-            <span className="text-white">1 Month</span> Account
+            <span className="text-white">30%</span> Will succeed{" "}
+            <span className="text-white">6 Month</span> Enough
           </p>
         </div>
 
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span>{" "}
+            The Best <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Platform4</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
@@ -25,12 +25,10 @@ const Hero = () => {
         </div>
 
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Payment Method.
+        Excellent student.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs. We examine annual percentage rates,
-          annual fees.
+        Our team of experts uses a methodology to develop students' skills and their ability to solve difficult questions and questions of higher levels of thinking in scientific subjects to train them on the new system.
         </p>
       </div>
 
